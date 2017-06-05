2f69e3c59ec1b8699ac71e299b5adc1735bb94be
Lee Hankins <bryanhankins@me.com>
2017-06-05 08:24:09 -0700

    added customer class

---
+++ lee-hankins-backend-capstone/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using lee_hankins_backend_capstone.Controllers.Interfaces;
+using lee_hankins_backend_capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace lee_hankins_backend_capstone.Controllers
+{
+    [RoutePrefix("api/customer")]
+    public class CustomerController : ApiController
+    {
+        readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpPost, Route]
+        public HttpResponseMessage RegisterCustomer(Customer customer)
+        {
+            if (string.IsNullOrWhiteSpace(customer.UserName))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid Username");
+            }
+
+            _customerRepository.Save(customer);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpGet, Route]
+        public HttpResponseMessage GetAll()
+        {
+            var customers = _customerRepository.GetAll();
+
+            return Request.CreateResponse(HttpStatusCode.OK, customers);
+        }
+    }
+}
---
+++ lee-hankins-backend-capstone/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lee_hankins_backend_capstone.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public string Phone { get; set; }
+    }
+}
